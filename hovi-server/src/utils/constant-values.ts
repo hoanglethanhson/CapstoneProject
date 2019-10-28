67051c36e5bceff829391cc880b3da80cfaab19b
Son Le <leson1998ls@gmail.com>
2019-10-28 21:00:07 +0700

    add constant values file

---
+++ hovi-server/src/utils/constant-values.ts
@@ -0,0 +1,6 @@
+export class ConstantValues {
+    static readonly DEFAULT_EMAIL = 'example@homohouse.vn';
+    static readonly DEFAULT_ADDRESS = 'not yet';
+    static readonly DEFAULT_FACEBOOK_ID = 'example-facebook-id';
+    static readonly DEFAULT_GOOGLE_ID = 'example-google-id';
+}
