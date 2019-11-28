8989b30e5e472a212d5ab18d67ff4403c0a53489
Nguyen Nhu Thuong <thuongnn6666@gmail.com>
2019-11-28 20:10:58 +0700

    add reset password

--- hovi-server/src/routes/auth-route.ts
+++ hovi-server/src/routes/auth-route.ts
@@ -1,19 +1,29 @@
 import AuthFunction from '../functions/auth-function';
 
 export default [
-  {
-    path: '/auth/register',
-    method: 'post',
-    handler: AuthFunction.createUser,
-  },
-  {
-    path: '/auth/verifyPhoneNumber',
-    method: 'post',
-    handler: AuthFunction.verifyPhoneNumber,
-  },
-  {
-    path: '/auth/login',
-    method: 'post',
-    handler: AuthFunction.signIn,
-  },
+    {
+        path: '/auth/register',
+        method: 'post',
+        handler: AuthFunction.createUser,
+    },
+    {
+        path: '/auth/verifyPhoneNumber',
+        method: 'post',
+        handler: AuthFunction.verifyPhoneNumber,
+    },
+    {
+        path: '/auth/login',
+        method: 'post',
+        handler: AuthFunction.signIn,
+    },
+    {
+        path: '/auth/forgot-password',
+        method: 'post',
+        handler: AuthFunction.sendResetPasswordEmail,
+    },
+    {
+        path: '/auth/reset-password',
+        method: 'post',
+        handler: AuthFunction.resetPassword,
+    },
 ];
