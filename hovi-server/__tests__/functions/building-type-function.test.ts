5cac3e7ff685ed1eb390a7bf16d5ded4afaf99a2
Nguyen Nhu Thuong <thuongnn6666@gmail.com>
2019-12-02 12:12:18 +0700

    update

--- hovi-server/__tests__/functions/building-type-function.test.ts
+++ hovi-server/__tests__/functions/building-type-function.test.ts
@@ -1,42 +1,45 @@
 import createServer from './server';
 import AuthFunction from "../../src/functions/auth-function";
 
-const request = require('supertest');
+const supertest = require('supertest');
 
 describe('Test function building type', () => {
-    let api;
+    let request = null;
+    let server = null;
     let token;
 
-    beforeEach(async () => {
-        api = await createServer();
+    beforeEach(async (done) => {
+        server = await createServer();
+        server.listen(done);
+        request = supertest.agent(server);
         token = await AuthFunction.getIdToken('+84986352227');
     });
 
+    afterAll((done) => {
+        request.close(done);
+    });
+
     it('Test get all building type', () => {
-        return request(api)
+        request
             .get('/buildingType')
             .then(response => {
                 expect(response.status).toBe(200);
-            })
+            });
     });
 
-    it('Test get all saved room of user not authentication', (done) => {
-        request(api)
+    it('Test get all saved room of user not authentication', () => {
+        request
             .get('/saved-rooms')
             .set('Accept', 'application/json')
             .expect(401)
             .expect('{"status":401,"message":"Unauthorized"}')
-            .end((err) => {
-                if (err) return done(err);
-                done();
-            });
     });
 
-    it('Test get all saved room of user', (done) => {
-        request(api)
+    it('Test get all saved room of user', () => {
+        request
             .get('/saved-rooms')
             .set('Authorization', token)
             .set('Accept', 'application/json')
-            .expect(200, done)
+            .expect(200)
     });
 });
